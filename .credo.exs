df57132ff7c46708664afd6434279922925439be
valvazoripaulo <valvazori.paulo@gmail.com>
2022-04-15 15:08:55 -0300

    configuração pipeline CI

---
+++ .credo.exs
@@ -0,0 +1,11 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{included: ~w{config lib test}},
+      strict: true,
+      color: true,
+      checks: [{Credo.Check.Warning.LazyLogging, false}]
+    }
+  ]
+}

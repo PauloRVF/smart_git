fefccb650cb5da7aa07a787459e0ec8773e78109
valvazoripaulo <valvazori.paulo@gmail.com>
2022-04-15 15:30:36 -0300

    fix pipeline

--- .credo.exs
+++ .credo.exs
@@ -5,7 +5,15 @@
       files: %{included: ~w{config lib test}},
       strict: true,
       color: true,
-      checks: [{Credo.Check.Warning.LazyLogging, false}]
+      checks: [
+        {Credo.Check.Readability.ModuleDoc, false},
+        {Credo.Check.Readability.MaxLineLength, max_length: 100},
+        {Credo.Check.Consistency.TabsOrSpaces},
+        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
+        {Credo.Check.Design.TagTODO, exit_status: 2},
+        {Credo.Check.Refactor.MapInto, false},
+        {Credo.Check.Warning.LazyLogging, false}
+      ]
     }
   ]
 }
